972e334db9dea63c26d5ce04b19ac3b73a83fdf2
haley hopper <143463844+Hop1102@users.noreply.github.com>
2023-12-07 18:20:46 +0000

    question b-Multiplication table

--- src/question_b/main.py
+++ src/question_b/main.py
@@ -1,1 +1,32 @@
 #add import
+
+from question_b import display_multiplication_table, create_multiplication_table
+
+def main():
+    while True:
+        print("Main menu:")
+        print("1) Generate/display multiplication table")
+        print("2) Quit")
+
+        choice = input("Enter your choice(1/2): ")
+
+        if choice == "1":
+            rows = int(input("Enter the number of rows: "))
+            cols = int(input("Enter the number of columns: ")) 
+
+            multiplication_table = create_multiplication_table(rows, cols)
+            display_multiplication_table(multiplication_table)
+
+        elif choice == "2":
+            print("Exiting program. Goodbye.") 
+            break 
+        else:
+            print("Invalid response. please put 1 or 2.") 
+
+if __name__ == "__main__":
+    main() 
+            
+
+
+        
+    
--- src/question_b/question_b.py
+++ src/question_b/question_b.py
@@ -1,1 +1,18 @@
 #write functions here, don't add input('') statements here!
+def create_multiplication_table(rows, cols):
+    table = []
+    for i in range (1, rows + 1):
+        row = []
+        for j in range(1, cols + 1):
+            row.append(i + j)
+        table.append(row)
+    return table
+
+def display_multiplication_table(table):
+    for row in table:
+        for value in row:
+            print(f"{value:4}", end=" ")
+        print()
+
+
+        
